0de4e5036954011b8d2b0e2784b01538a13a97ea
GentiLuc <99353440+GentiLuc@users.noreply.github.com>
2022-07-25 14:33:18 -0300

    Subo header

---
+++ Proyecto_Gentile/src/app/components/logo-yp/logo-yp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-yp',
+  templateUrl: './logo-yp.component.html',
+  styleUrls: ['./logo-yp.component.css']
+})
+export class LogoYPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

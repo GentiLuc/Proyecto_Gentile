c5fab778ee2076084ac820cde8110c71c57c44fa
GentiLuc <99353440+GentiLuc@users.noreply.github.com>
2022-07-28 14:08:00 -0300

    creo proyectos

--- Proyecto_Gentile/src/app/app.module.ts
+++ Proyecto_Gentile/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HssComponent } from './components/hss/hss.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
  
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HssComponent } from './components/hss/hss.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HssComponent
+    HssComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
